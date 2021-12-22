1b175a76fb797120caab786653e21f68487cc2d9
Creatodidak <ppidpolreslandak@gmail.com>
2021-12-23 01:30:22 +0700

    1.30

--- app/src/main/java/com/creatodidak/lapharhumas/ApiEndPoint.kt
+++ app/src/main/java/com/creatodidak/lapharhumas/ApiEndPoint.kt
@@ -2,7 +2,7 @@ package com.creatodidak.lapharhumas
 
 class ApiEndPoint {companion object {
 
-    private val SERVER = "http://10.21.210.135/crud/"
+    private const val SERVER = "http://0ee0-36-95-141-130.ngrok.io/crud/"
     val CREATE = SERVER+"create.php"
     val READ = SERVER+"read.php"
     val DELETE = SERVER+"delete.php"
